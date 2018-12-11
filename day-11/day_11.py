af600912d22b6daae991a21921e09c1c369facbb
Neil Thistlethwaite <11429656+nthistle@users.noreply.github.com>
2018-12-11 01:33:37 -0500

    Refactor to use builtin lists instead of numpy
    Gives ~40% speedup because of overhead in accessing numpy arrays

--- day-11/day_11.py
+++ day-11/day_11.py
@@ -1,22 +1,20 @@
-import numpy as np
-
 grid_serial = 6392
 
-fuel = np.zeros((300,300))
-rsum = np.zeros((301,301))
+fuel = [[0 for _ in range(300)] for _ in range(300)]
+rsum = [[0 for _ in range(301)] for _ in range(301)]
 
 for i in range(300):
     for j in range(300):
-        fuel[i,j] = (((i+11)*(j+1)+grid_serial)*(i+11))//100%10-5
+        fuel[i][j] = (((i+11)*(j+1)+grid_serial)*(i+11))//100%10-5
 
 for i in range(300):
     for j in range(300):
-        rsum[i+1,j+1] += fuel[i,j]
-        rsum[i+1,j+1] += rsum[i,j+1]
-        rsum[i+1,j+1] += rsum[i+1,j]
-        rsum[i+1,j+1] -= rsum[i,j]
+        rsum[i+1][j+1] += fuel[i][j]
+        rsum[i+1][j+1] += rsum[i][j+1]
+        rsum[i+1][j+1] += rsum[i+1][j]
+        rsum[i+1][j+1] -= rsum[i][j]
 
-calc_sum = lambda x,y,s,r : r[x+s,y+s]-r[x,y+s]-r[x+s,y]+r[x,y]
+calc_sum = lambda x,y,s,r : r[x+s][y+s]-r[x][y+s]-r[x+s][y]+r[x][y]
 
 print("Part 1:",",".join(map(str,max(((x+1,y+1) for x in range(300-2) for y in range(300-2)), \
                     key=lambda p:calc_sum(p[0]-1,p[1]-1,3,rsum)))))
